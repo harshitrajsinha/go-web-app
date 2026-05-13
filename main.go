f0bc1992622462a25e0e41130ac188da63b06f5a
Harshit Raj Sinha <raj.harshitsinha08@gmail.com>
2026-05-13 15:12:14 +0000

    containerizing app

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	http.HandleFunc("/home", homePage)
-	http.HandleFunc("/courses", coursePage)
+	http.HandleFunc("/", coursePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
 
